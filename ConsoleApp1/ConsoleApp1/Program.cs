8b6d276357520b8d3caddda8fcb576aef04d4e98
Kamlesh Solanki <kamlesh.solanki@yash.com>
2021-04-23 16:47:53 +0530

    newch

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -10,6 +10,12 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+           
+        }
+        public int sum()
+        {
+            return 0;
         }
     }
+
 }
